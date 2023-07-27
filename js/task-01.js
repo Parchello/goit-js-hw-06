8085c1d53cca1ab0c2c811b1c99ebf1222f4131c
Parchello <parchellos@gmail.com>
2023-07-27 16:46:55 +0300

    start

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const list = document.querySelector("ul#categories");
+console.log(list);
+
+const items = document.querySelectorAll(".item");
+console.log(items);
+console.log([...items]);
+console.dir(items);
