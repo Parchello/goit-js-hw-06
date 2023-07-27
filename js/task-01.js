652221cd35c6a871adab223f4c1d0340df0b06fb
Parchello <parchellos@gmail.com>
2023-07-27 18:11:00 +0300

    task 2

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,9 @@ const list = document.querySelector("ul#categories");
 console.log(list);
 
 const items = document.querySelectorAll(".item");
-console.log(items);
-console.log([...items]);
+console.dir(items[0].textContent);
+console.log(
+  `elements: ${items[0].children.length}`,
+  `categories: ${items[1].children.length}`
+);
 console.dir(items);
